443c487f54a4d55a3523ef8e600e06207fda9774
Tamilvani <limatinav2005@gmail.com>
2026-02-13 08:23:05 +0530

    MongoDB name

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use("/admin", require("./routes/admin"));
 app.use("/player", require("./routes/player"));
 
 // MongoDB connect
-mongoose.connect("mongodb://127.0.0.1:27017/arena")
+mongoose.connect("mongodb://127.0.0.1:27017/TileMatch")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
